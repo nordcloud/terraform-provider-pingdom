26080ab95ea10acedb1511b47033d89c6d81496c
chszchen <chszchen@cn.ibm.com>
2021-04-06 19:25:25 +0800

    Minor fix after merging

--- pingdom/provider_test.go
+++ pingdom/provider_test.go
@@ -5,8 +5,8 @@ import (
 	"os"
 	"testing"
 
-	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
-	"github.com/hashicorp/terraform-plugin-sdk/terraform"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 )
 
 var testAccProviders map[string]*schema.Provider
@@ -59,12 +59,6 @@ func TestProviderConfigure(t *testing.T) {
 		if config.APIToken != expectedToken {
 			t.Fatalf("bad: %#v", config)
 		}
-	}
-}
-
-func testAccPreCheck(t *testing.T) {
-	if v := os.Getenv("PINGDOM_API_TOKEN"); v == "" {
-		t.Fatal("PINGDOM_API_TOKEN environment variable must be set for acceptance tests")
 	}
 }
 
